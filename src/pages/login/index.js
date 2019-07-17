6428ed854b4b8d63d63c12c16b469ecf57b87020
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-17 18:54:37 -0400

    adicionando estrutura do formulário de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { PrimaryButton, TextField } from 'office-ui-fabric-react';
 
+import { validateEmail } from '../../helpers';
 import Card from '../../components/card';
 
 import Style from './style.module.scss';
 
 
-const Login = () => (
-  <div className={Style.login}>
-    <Card className={Style.login__card}>
-      <h2 className={Style.login__logo}>AVALIAÇÃO MUSEU</h2>
-      <TextField type="email" placeholder="E-mail" iconProps={{ iconName: 'Mail' }} underlined />
-      <TextField type="password" placeholder="Senha" iconProps={{ iconName: 'Permissions' }} underlined />
-      <PrimaryButton className={Style.login__button}>Entrar</PrimaryButton>
-    </Card>
-  </div>
-);
+const Login = () => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleOnChange = ({ target }) => {
+    setInputs({
+      ...inputs,
+      [target.type]: target.value,
+    });
+  };
+
+  const handleOnClick = () => {
+    const { email, password } = inputs;
+    if (validateEmail(email) && password.length > 0) {
+      console.log('pode enviar');
+    }
+  };
+
+  return (
+    <div className={Style.login}>
+      <Card className={Style.login__card}>
+        <form className={Style.login__form}>
+          <h2 className={Style.login__logo}>AVALIAÇÃO MUSEU</h2>
+          <TextField
+            type="email"
+            placeholder="E-mail"
+            iconProps={{ iconName: 'Mail' }}
+            underlined
+            value={inputs.email}
+            onChange={handleOnChange}
+          />
+          <TextField
+            type="password"
+            placeholder="Senha"
+            iconProps={{ iconName: 'Permissions' }}
+            underlined
+            value={inputs.password}
+            onChange={handleOnChange}
+          />
+          <PrimaryButton className={Style.login__button} onClick={handleOnClick}>
+            Entrar
+          </PrimaryButton>
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 
 export default Login;
