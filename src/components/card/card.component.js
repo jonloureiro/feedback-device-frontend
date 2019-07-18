81f176f8218679872f8fa1e441018e2f3e10c354
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-18 14:18:07 -0400

    corrigindo className undefined

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Style from './card.module.scss';
 
 const Card = ({ children, className }) => (
-  <div className={`${Style.card} ${className}`}>
+  <div className={`${Style.card} ${className || ''}`}>
     { children }
   </div>
 );
