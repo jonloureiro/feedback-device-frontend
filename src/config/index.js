8fff6c1ea72764f3a5c82c2419b26276890876df
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2019-07-20 23:59:39 -0400

    configurando base url quando backend for localhost

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,10 @@
 /* eslint-disable import/prefer-default-export */
 
+const localhost = (process.env.REACT_APP_BASE_URL === 'localhost')
+  ? `${window.location.protocol}//${window.location.hostname}${process.env.REACT_APP_PORT || ''}`
+  : undefined;
 
-export const baseURL = process.env.REACT_APP_BASE_URL || 'https://jonloureiro-museu.herokuapp.com';
+const baseURL = localhost || process.env.REACT_APP_BASE_URL || 'https://jonloureiro-museu.herokuapp.com';
+
+
+export { baseURL };
